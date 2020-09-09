692e1e374f9fee7f954924a736ef05f446c92743
Prakash Shahi <prakashthakuri1994@gmail.com>
2020-09-09 14:01:20 -0400

    Working with components [selector, templateUrl, template, styleUrls and styles]

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // selector: '[app-servers]',
+
+  templateUrl: './servers.component.html',
+  // tempalte: '<h1>this is on template</h1> defining html file on the same file
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
